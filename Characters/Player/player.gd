99f05ddef63ec561e52ac7fc9ef97c25c9701d66
Maximus Katsoulis <maximuskatsoulis985@gmail.com>
2025-07-31 11:21:28 -0500

    Added basic character movement

---
+++ Characters/Player/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+@export var FRICTION: float = 0.0
+@export var ACCEL: float = 0.0
+@export var MAX_SPEED: float = 0.0
+var input_vector: Vector2 = Vector2.ZERO
+
+func _handle_input():
+	input_vector = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		input_vector.x = 1
+	elif Input.is_action_pressed("move_left"):
+		input_vector.x = -1
+	
+	if Input.is_action_pressed("move_down"):
+		input_vector.y = 1
+	elif Input.is_action_pressed("move_up"):
+		input_vector.y = -1
+	input_vector = input_vector.normalized()
+	
+	if sign(velocity.x) != sign(input_vector.x) and input_vector.x != 0:
+		velocity.x *= 0.77
+	
+	if sign(velocity.y) != sign(input_vector.y) and input_vector.y != 0:
+		velocity.y *= 0.77
+		
+func _physics_process(delta: float) -> void:
+	_handle_input()
+	if input_vector.x != 0.0:
+		velocity.x = move_toward(velocity.x, MAX_SPEED * input_vector.x, ACCEL * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
+		
+	if input_vector.y != 0.0:
+		velocity.y = move_toward(velocity.y, MAX_SPEED * input_vector.y, ACCEL * delta)
+	else:
+		velocity.y = move_toward(velocity.y, 0.0, FRICTION * delta)
+	print(velocity)
+	move_and_slide()
